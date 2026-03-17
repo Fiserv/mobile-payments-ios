9e4100bc5714723bef0d3dcd62db8521b7b02afe
Allan <allan3723@gmail.com>
2026-03-17 13:12:45 -0700

    Updated release

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "FiservMobilePayments",
-            url: "https://github.com/Fiserv/mobile-payments-ios/releases/download/1.0.20/FiservMobilePayments.xcframework.zip",
-            checksum: "2e0f2fdba1da760e9d59156350ea5c533c457c8f42954ee333dd93bd046505e3"
+            url: "https://github.com/Fiserv/mobile-payments-ios/releases/download/1.0.21/FiservMobilePayments.xcframework.zip",
+            checksum: "8853573369241157d550eb7700610648086bf78ab028a7d492de55289a1a23f8"
         )
     ]
 )
